a6776aca039d65d6dd91c0722684085c9df3f93e
Monty9120 <morph.man@hotmail.com>
2018-10-31 21:04:59 +1300

    1

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 $(function(){
 	console.log('jquery test')
 
+
 $('.currency').mask('000,000,000', {reverse: true});
 
-
-//Calculate Loan Amount
-$('#deposit, #purchase-price').on('input',function(){
+$('#refresh').on('click',function(){
+	//Calculate Loan Amount
 	var purchasePrice = $('#purchase-price').val().replace(/,/g, '');
-
-
 	var deposit = $('#deposit').val().replace(/,/g, '');
-
 	$('.loan-amount').html(purchasePrice-deposit)
-})
 
+	//Calculate Rental Income
+	var rentalIncome = $('#rental-income').val().replace(/,/g, '');
+	$('.r-weekly-rent').html(rentalIncome)
+
+
+	//Calculate annual rent and
+	//Calculate property managemnet
+	var annualVaccancy = $('#annual-vaccancy').val()
+	var rentalIncome = $('#rental-income').val().replace(/,/g, '');
+	$('.r-annual-rent').html(rentalIncome*(52-annualVaccancy));
+
+	annualRentVal = $('.r-annual-rent').html();
+	console.log(annualRentVal);
+
+	var propertyMangt = $ ('#property-management').val();
+	$('.r-property-mangt').html((annualRentVal*propertyMangt)/100);
 
 	
-})
+
+// annual rent * property mngment %
+	});
+});
