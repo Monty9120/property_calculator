314a720c91cd3f4f04bda511f1520c28eeae410e
montague michael llewelyn morphy <845873736@cit3-501m-14.acg.ac.nz>
2018-11-02 14:32:53 +1300

    added loop for yearly inc

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(function(){
 	console.log('jquery test')
 
 
-$('.currency').mask('000,000,000', {reverse: false});
+$('.currency').mask('000,000,000', {reverse: true});
 
 $('#purchase-price, #deposit').on('input',function(){
 	//Calculate Loan Amount
@@ -21,19 +21,38 @@ $('#refresh').on('click',function(){
 
 
 	//Calculate annual rent and
-	//Calculate property managemnet
+	
 	var purchasePrice = $('#purchase-price').val().replace(/,/g, '');
 	var annualVaccancy = $('#annual-vaccancy').val()
 	var rentalIncome = $('#rental-income').val().replace(/,/g, '');
 	$('.r-annual-rent').html(rentalIncome*(52-annualVaccancy));
+	// $('.loan-amount').html(purchasePrice-deposit)
 
-	$('.loan-amount').html(purchasePrice-deposit)
-
-
+	//Calculate property managemnet
 	annualRentVal = $('.r-annual-rent').html();
 	console.log(annualRentVal);
 	var propertyMangt = $ ('#property-management').val();
 	$('.r-property-mangt').html((annualRentVal*propertyMangt)/100);
+
+
+
+
+
+
+
+	// Calculate rental increase
+	var cell = $('.r-weekly-rent');
+	var weeklyRent = parseFloat($('.r-weekly-rent').text().replace(/$/g, ''));
+	var increateRate = 0.03;
+
+	while(cell.next().length>0){
+
+		weeklyRent += weeklyRent*increateRate;
+		cell = cell.next();
+		cell.text(weeklyRent.toFixed(2));
+	}
+
+	
 
 });
 
@@ -42,5 +61,5 @@ $('#refresh').on('click',function(){
 
 	
 
-// annual rent * property mngment %
-	});
+
+});
