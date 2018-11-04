142285797f4d5fcfc8f4f2cfbc5f73381a850d40
Monty9120 <morph.man@hotmail.com>
2018-11-04 19:13:08 +1300

    suunday work

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ $(function(){
 	console.log('jquery test')
 
 
-$('.currency').mask('000,000,000', {reverse: true});
+	$('.currency').mask('000,000,000', {reverse: true});
 
-$('#purchase-price, #deposit').on('input',function(){
-	//Calculate Loan Amount
-	var purchasePrice = $('#purchase-price').val().replace(/,/g, '');
-	var deposit = $('#deposit').val().replace(/,/g, '');
-	$('.loan-amount').html(purchasePrice-deposit)
+	$('#purchase-price, #deposit').on('input',function(){
+		//Calculate Loan Amount
+		var purchasePrice = $('#purchase-price').val().replace(/,/g, '');
+		var deposit = $('#deposit').val().replace(/,/g, '');
+		var loanAmount = $('.loan-amount').html(purchasePrice-deposit)
 
 });
 
@@ -32,27 +32,61 @@ $('#refresh').on('click',function(){
 	annualRentVal = $('.r-annual-rent').html();
 	console.log(annualRentVal);
 	var propertyMangt = $ ('#property-management').val();
-	$('.r-property-mangt').html((annualRentVal*propertyMangt)/100);
-
-
-
-
+	var propertyMangtResult = $('.r-property-mangt').html((annualRentVal*propertyMangt)/100);
 
 
 
 	// Calculate rental increase
-	var cell = $('.r-weekly-rent');
+	var weeklyRentCell = $('.r-weekly-rent');
 	var weeklyRent = parseFloat($('.r-weekly-rent').text().replace(/$/g, ''));
-	var increateRate = 0.03;
+	var increateRate = $('#rental-increase').val()/100;
 
-	while(cell.next().length>0){
+	while(weeklyRentCell.next().length>0){
 
 		weeklyRent += weeklyRent*increateRate;
-		cell = cell.next();
-		cell.text(weeklyRent.toFixed(2));
+		weeklyRentCell = weeklyRentCell.next();
+		weeklyRentCell.text(weeklyRent.toFixed(2));
 	}
 
 	
+
+	//Calcualte water and rates
+	var water = $('#water').val().replace(/,/g, '');
+	var rates = $('#rates').val().replace(/,/g, '');
+	//NEEDS TO BE NUMBER NOT STRING!!!!!!
+	var ratesWaterResult = parseFloat($('.r-rates-water').html((water+rates)));
+
+	//Body coroperate
+	var bodyCorpVal =$('#body-corporate').val();
+	$('.r-body-corp').text('$'+bodyCorpVal);
+
+	//Insurance Calc
+	var insuranceVal =$('#insurance').val();
+	$('.r-insurance').text('$'+insuranceVal);
+
+
+	//Maintenance
+	var maintenanceValUnder10 =$('#maintenance-underten').val();
+	var maintenanceResult = $('.r-maintenance').text('$'+annualRentVal*(maintenanceValUnder10/100));
+
+
+	// Annual Costs
+	var annualCost = (maintenanceResult+insuranceVal+bodyCorpVal+ratesWaterResult+propertyMangtResult);
+	var annualCostResult = $('.r-annual-costs').text(annualCost);
+
+
+	//Annual interest
+	var purchasePrice = $('#purchase-price').val().replace(/,/g, '');
+		var deposit = $('#deposit').val().replace(/,/g, '');
+		var loanAmount = $('.loan-amount').html(purchasePrice-deposit);
+	var annualInterestVal = $('#annual-interest-rate').val();
+	console.log(loanAmount.html());
+
+	var annualInterestResult  = $('.r-annual-interest').html('$'+(loanAmount.html()*(annualInterestVal/100))*-1);
+
+
+	//Total expenses
+	var totalExpenses = (annualInterestResult.html()-annualCostResult.html())
 
 });
 
